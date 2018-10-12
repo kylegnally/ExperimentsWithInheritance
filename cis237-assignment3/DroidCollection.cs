5ceced6c946aed0b0d048cacae7c5acdb7162cde
Kyle Nally <kylegnally@gmail.com>
2018-10-12 10:49:36 -0400

    Begin adding code to Droid collection.

--- cis237-assignment3/DroidCollection.cs
+++ cis237-assignment3/DroidCollection.cs
@@ -8,5 +8,45 @@ namespace cis237_assignment3
 {
     class DroidCollection
     {
+        Droid[] droids;
+        int collectionPosition;
+
+        /// <summary>
+        /// Creates a new droids array of type Beverage and of the size passed into it.
+        /// Sets the initial position of the array counter to 0 (the beginning of the array).
+        /// </summary>
+        /// <param name="collectionSize"></param>
+        public DroidCollection(int collectionSize)
+        {
+            droids = new Droid[collectionSize];
+            collectionPosition = 0;
+        }
+        
+        // There should be four Add methods here - one for each Droid type
+        //public void Add(string id, string desc, string pack, decimal price, bool active)
+        //{
+        //    droids[collectionPosition] = new Droid(id, desc, pack, price, active);
+        //    collectionPosition++;
+        //}
+
+        /// <summary>
+        /// Prints the entire inventory of droids using the ToString() override method of the Beverage class.
+        /// Returns the entire inventory as a string[].
+        /// </summary>
+        /// <returns>string[]</returns>
+        public string[] PrintTheDroidsInventory()
+        {
+            string[] allDroids = new string[collectionPosition];
+
+            for (int i = 0; i < droids.Length; i++)
+            {
+                if (droids[i] != null)
+                {
+                    allDroids[i] = "\t\t\t\t" + droids[i].ToString();
+                }
+            }
+
+            return allDroids;
+        }
     }
 }
